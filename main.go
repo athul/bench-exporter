57032bfcc0bf5ab4a38baf432d3dc6f3f1a20885
athul <athul8720@gmail.com>
2022-10-21 18:14:50 +0000

    feat: Implement user flags for Bench folder

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -8,10 +9,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var benchDir string
+var userCounts bool
+
 func main() {
-  // TODO:improve this whole thing
+	benchPath := flag.String("bench", "/home/frappe/frappe-bench", "Path to Bench Directory")
+	usersCounts := flag.Bool("users", false, "Perform SQL operations to fetch user counts")
+	flag.Parse()
+	benchDir = *benchPath
+	userCounts = *usersCounts
+
 	benchCollector := newBenchMetrics()
 	prometheus.MustRegister(benchCollector)
 	http.Handle("/metrics", promhttp.Handler())
-	log.Println(http.ListenAndServe(":9101", nil))
+
+	log.Printf("INFO: Starting http server - %s", "0.0.0.0:9101")
+	if err := http.ListenAndServe(":9101", nil); err != nil {
+		log.Fatalf("ERROR: Failed to start http server: %s", err)
+	}
 }
--- users.go
+++ users.go
@@ -7,13 +7,12 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// getUserCountonSite returns the count for allusers, activeusers and system managers 
+// getUserCountonSite returns the count for allusers, activeusers and system managers
 // uses direct SQL queries.
-func getUserCountonSite(siteName string)(float64,float64,float64) {
+func getUserCountonSite(siteName string) (float64, float64, float64) {
 	allUserQuery := `SELECT COUNT(name) FROM tabUser WHERE enabled=1 AND user_type != 'Website User' AND name NOT IN ("Administrator","Guest");`
 	activeUserQuery := `SELECT COUNT(*) FROM tabUser WHERE enabled=1 AND user_type != 'Website User' AND name NOT IN ("Administrator","Guest") AND hour(timediff(now(), last_active)) < 72;`
 	systemManagerQuery := "SELECT DISTINCT COUNT(name) FROM `tabUser` AS p WHERE enabled=1 AND docstatus<2 AND name NOT IN (\"Administrator\",\"Guest\") AND EXISTS(SELECT * FROM `tabHas Role` AS ur WHERE ur.parent=p.name AND ur.role=\"System Manager\");"
-	log.Println("Usercount func:",siteName)
 	db, err := sql.Open("mysql", generateDbURI(siteName))
 	defer db.Close()
 	if err != nil {
@@ -35,20 +34,14 @@ func getUserCountonSite(siteName string)(float64,float64,float64) {
 		log.Println("Preparing systemManagerQuery failed ", err)
 	}
 	var activeUsers, systemManagers, allUsers float64
-
 	if err := allUserQuerystmt.QueryRow().Scan(&allUsers); err != nil {
 		log.Println("AllUserQuery failed ", err)
 	}
-
 	if err := activeUserQuerystmt.QueryRow().Scan(&activeUsers); err != nil {
 		log.Println("activeUserQuery failed", err)
 	}
-
 	if err := systemManagerQuerystmt.QueryRow().Scan(&systemManagers); err != nil {
 		log.Println("systemManagerQuery failed ", err)
 	}
-
-
-	return allUsers,activeUsers,systemManagers
-
+	return allUsers, activeUsers, systemManagers
 }
